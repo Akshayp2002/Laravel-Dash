d4011d193bf05ad03911c5ce2ac9254f6961c41d
Akshay <akshay@cloudrevelinnovation.com>
2025-02-01 17:04:16 +0530

    dashboard

---
+++ bootstrap/providers.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    App\Providers\AppServiceProvider::class,
+    App\Providers\FortifyServiceProvider::class,
+    App\Providers\HorizonServiceProvider::class,
+    App\Providers\JetstreamServiceProvider::class,
+    App\Providers\TelescopeServiceProvider::class,
+    OwenIt\Auditing\AuditingServiceProvider::class,
+];
